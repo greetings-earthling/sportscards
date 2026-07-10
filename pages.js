1dd34e234c57b712775104b229ac7090b1e72333
greetings-earthling <houledennis@gmail.com>
2026-07-10 12:15:43 -0400

    Update pages.js

--- pages.js
+++ pages.js
@@ -3,30 +3,65 @@ const pages = [
     image: "images/page001.jpg",
     stickers: [
       { text: "$2", class: "s1" },
-      { text: "RC", color: "red", class: "s2" },
+      { text: "RC", class: "s2", color: "red" },
       { text: "$3", class: "s3" },
       { text: "FT", class: "s4" },
       { text: "$1", class: "s5" },
-      { text: "PC?", class: "s6" },
+      { text: "PC?", class: "s6", color: "purple" },
       { text: "$2", class: "s7" },
-      { text: "RC", class: "s8" },
+      { text: "RC", class: "s8", color: "red" },
       { text: "$5", class: "s9" }
+    ],
+    cards: [
+      { class: "c1", name: "Page 1, Card 1" },
+      { class: "c2", name: "Page 1, Card 2" },
+      { class: "c3", name: "Page 1, Card 3" },
+      { class: "c4", name: "Page 1, Card 4" },
+      { class: "c5", name: "Page 1, Card 5" },
+      { class: "c6", name: "Page 1, Card 6" },
+      { class: "c7", name: "Page 1, Card 7" },
+      { class: "c8", name: "Page 1, Card 8" },
+      { class: "c9", name: "Page 1, Card 9" }
     ]
   },
+
   {
     image: "images/page002.jpg",
     stickers: [
       { text: "$5", class: "s1" },
-      { text: "/99", class: "s3" },
-      { text: "AUTO", class: "s5" }
+      { text: "/99", class: "s3", color: "blue" },
+      { text: "AUTO", class: "s5", color: "green" }
+    ],
+    cards: [
+      { class: "c1", name: "Page 2, Card 1" },
+      { class: "c2", name: "Page 2, Card 2" },
+      { class: "c3", name: "Page 2, Card 3" },
+      { class: "c4", name: "Page 2, Card 4" },
+      { class: "c5", name: "Page 2, Card 5" },
+      { class: "c6", name: "Page 2, Card 6" },
+      { class: "c7", name: "Page 2, Card 7" },
+      { class: "c8", name: "Page 2, Card 8" },
+      { class: "c9", name: "Page 2, Card 9" }
     ]
   },
+
   {
     image: "images/page003.jpg",
     stickers: [
-      { text: "PC", class: "s2" },
-      { text: "RC", class: "s4" },
+      { text: "PC", class: "s2", color: "purple" },
+      { text: "RC", class: "s4", color: "red" },
       { text: "$10", class: "s9" }
+    ],
+    cards: [
+      { class: "c1", name: "Page 3, Card 1" },
+      { class: "c2", name: "Page 3, Card 2" },
+      { class: "c3", name: "Page 3, Card 3" },
+      { class: "c4", name: "Page 3, Card 4" },
+      { class: "c5", name: "Page 3, Card 5" },
+      { class: "c6", name: "Page 3, Card 6" },
+      { class: "c7", name: "Page 3, Card 7" },
+      { class: "c8", name: "Page 3, Card 8" },
+      { class: "c9", name: "Page 3, Card 9" }
     ]
   }
 ];
