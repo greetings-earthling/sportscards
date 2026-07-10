45b94c18ae5eeb1e95deef842ce94102f4a98a44
greetings-earthling <houledennis@gmail.com>
2026-07-10 13:41:55 -0400

    Update pages.js

--- pages.js
+++ pages.js
@@ -3,25 +3,25 @@ const pages = [
     image: "images/page001.jpg",
     stickers: [
       { text: "$2", class: "s1" },
-      { text: "RC", class: "s2", color: "red" },
-      { text: "$3", class: "s3" },
-      { text: "FT", class: "s4" },
-      { text: "$1", class: "s5" },
-      { text: "PC?", class: "s6", color: "purple" },
-      { text: "$2", class: "s7" },
-      { text: "RC", class: "s8", color: "red" },
-      { text: "$5", class: "s9" }
+      { text: "$5 variation", class: "s2", color: "orange" },
+      { text: "$5 variation", class: "s3", color: "orange" },
+      { text: "$5 variation", class: "s4", color: "orange" },
+      { text: "$3", class: "s5" },
+      { text: "$3 2nd year", class: "s6" },
+      { text: "$3", class: "s7" },
+      { text: "$7 2nd year", class: "s8", color: "red" },
+      { text: "$3 2nd year", class: "s9" }
     ],
     cards: [
-      { class: "c1", name: "Page 1, Card 1" },
-      { class: "c2", name: "Page 1, Card 2" },
-      { class: "c3", name: "Page 1, Card 3" },
-      { class: "c4", name: "Page 1, Card 4" },
-      { class: "c5", name: "Page 1, Card 5" },
-      { class: "c6", name: "Page 1, Card 6" },
-      { class: "c7", name: "Page 1, Card 7" },
-      { class: "c8", name: "Page 1, Card 8" },
-      { class: "c9", name: "Page 1, Card 9" }
+      { class: "c1", name: "Trout 1,1" },
+      { class: "c2", name: "Trout 1,2" },
+      { class: "c3", name: "Trout 1,3" },
+      { class: "c4", name: "Trout 1,4" },
+      { class: "c5", name: "Griffey 1,5" },
+      { class: "c6", name: "Griffey 1,6" },
+      { class: "c7", name: "Griffey 1,7" },
+      { class: "c8", name: "Griffey 1,8" },
+      { class: "c9", name: "Griffey 1,9" }
     ]
   },
 
