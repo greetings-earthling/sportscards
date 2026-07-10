3290bec6628691d88c5ea84b99a755670ba922a7
greetings-earthling <houledennis@gmail.com>
2026-07-10 11:43:50 -0400

    Create pages.js

---
+++ pages.js
@@ -0,0 +1,32 @@
+const pages = [
+  {
+    image: "images/page001.jpg",
+    stickers: [
+      { text: "$2", class: "s1" },
+      { text: "RC", class: "s2" },
+      { text: "$3", class: "s3" },
+      { text: "FT", class: "s4" },
+      { text: "$1", class: "s5" },
+      { text: "PC?", class: "s6" },
+      { text: "$2", class: "s7" },
+      { text: "RC", class: "s8" },
+      { text: "$5", class: "s9" }
+    ]
+  },
+  {
+    image: "images/page002.jpg",
+    stickers: [
+      { text: "$5", class: "s1" },
+      { text: "/99", class: "s3" },
+      { text: "AUTO", class: "s5" }
+    ]
+  },
+  {
+    image: "images/page003.jpg",
+    stickers: [
+      { text: "PC", class: "s2" },
+      { text: "RC", class: "s4" },
+      { text: "$10", class: "s9" }
+    ]
+  }
+];
