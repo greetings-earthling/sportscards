a8c52976ebfda57108fb6c13d6e1bb07a9a56aa8
greetings-earthling <houledennis@gmail.com>
2026-07-10 12:06:07 -0400

    Update script.js

--- script.js
+++ script.js
@@ -4,36 +4,32 @@ function updatePage() {
   const page = pages[currentPage];
 
   document.getElementById("binderImage").src = page.image;
-  document.getElementById("pageNumber").innerText = `Page ${currentPage + 1} of ${pages.length}`;
+  document.getElementById("pageNumber").innerText =
+    `Page ${currentPage + 1} of ${pages.length}`;
 
   const stickerLayer = document.getElementById("stickerLayer");
   stickerLayer.innerHTML = "";
 
   page.stickers.forEach(sticker => {
 
-  const div = document.createElement("div");
+    const div = document.createElement("div");
 
-  div.className = `sticker ${sticker.class}`;
+    div.className = `sticker ${sticker.class}`;
 
-  if (sticker.color) {
+    if (sticker.color) {
+      div.classList.add(sticker.color);
+    }
 
-    div.classList.add(sticker.color);
+    div.innerText = sticker.text;
 
-  }
+    div.addEventListener("click", (e) => {
+      e.stopPropagation();
+      div.classList.toggle("hidden");
+    });
 
-  div.innerText = sticker.text;
-
-  div.addEventListener("click", () => {
-
-    div.classList.toggle("hidden");
+    stickerLayer.appendChild(div);
 
   });
-
-  stickerLayer.appendChild(div);
-
-});
-
-  makeStickersDraggable();
 }
 
 function nextPage() {
@@ -56,35 +52,11 @@ const lightboxImage = document.getElementById("lightboxImage");
 
 binderImage.addEventListener("click", () => {
   lightboxImage.src = binderImage.src;
-  lightbox.style.display = "block";
+  lightbox.classList.add("open");
 });
 
 lightbox.addEventListener("click", () => {
-  lightbox.style.display = "none";
+  lightbox.classList.remove("open");
 });
-
-function makeStickersDraggable() {
-  document.querySelectorAll(".sticker").forEach(sticker => {
-    sticker.onpointerdown = e => {
-      sticker.setPointerCapture(e.pointerId);
-
-      const startX = e.clientX;
-      const startY = e.clientY;
-      const startLeft = sticker.offsetLeft;
-      const startTop = sticker.offsetTop;
-
-      sticker.onpointermove = e => {
-        sticker.style.left = startLeft + (e.clientX - startX) + "px";
-        sticker.style.top = startTop + (e.clientY - startY) + "px";
-        sticker.style.right = "auto";
-        sticker.style.bottom = "auto";
-      };
-
-      sticker.onpointerup = () => {
-        sticker.onpointermove = null;
-      };
-    };
-  });
-}
 
 updatePage();
